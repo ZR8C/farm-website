21c0cdc1fa48b540ea7cc7bd552d0436f1c27123
zr8c <37085080+ZR8C@users.noreply.github.com>
2020-04-26 09:37:41 -0300

    Updates

---
+++ precache-manifest.4e002106f8162539d81173bc5bac7c5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e17d8a79cd7246d8d85075d2094e60",
+    "url": "/farm-website/index.html"
+  },
+  {
+    "revision": "c694981d7b557e9e37cf",
+    "url": "/farm-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6f9b488256cea8b16408",
+    "url": "/farm-website/static/js/2.2a6f587a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/farm-website/static/js/2.2a6f587a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c694981d7b557e9e37cf",
+    "url": "/farm-website/static/js/main.0bf4afd5.chunk.js"
+  },
+  {
+    "revision": "fda3acdc5488a586e116",
+    "url": "/farm-website/static/js/runtime-main.4dcd9827.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/farm-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
