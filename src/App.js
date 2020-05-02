ccbeef30effbf947d50547038197805b5c7a1d26
dave <david.goldfinch@datumsolutions.net>
2020-05-02 14:08:32 -0300

    add logo, load chickens json locally so gh pages works, add routes, fix css for navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import './App.css';
-import Chickenshelf from "./component/chickenshelf"
+import EggShelf from "./component/eggshelf"
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link,
+    Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <Chickenshelf />
-    </div>
+      <Router>
+          <div className="home">
+              <div className="header-container">
+                  <img className="logo" src={"logo.png"} alt="logo"/>
+
+                  <nav>
+                      <ul>
+                          <li>
+                              <Link to="/">Home</Link>
+                          </li>
+                          <li>
+                              <Link to="/eggs">Eggs</Link>
+                          </li>
+                          <li>
+                              <Link to="/about">About</Link>
+                          </li>
+                      </ul>
+                  </nav>
+              </div>
+
+
+
+              <Switch>
+                  <Route path="/eggs" component={EggShelf}>
+                  </Route>
+
+                  <Route path="/"></Route>
+                  <Route path="/about"></Route>
+              </Switch>
+          </div>
+      </Router>
+    // <div className="App">
+    // </div>
   );
 }
 
---
+++ src/component/eggshelf.js
@@ -0,0 +1,63 @@
+import Chicken from "./chicken";
+import React from "react";
+
+import data from './chickens.json'
+
+class EggShelf extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        }
+    }
+
+    componentDidMount() {
+        //  to enable node backend uncomment this
+        // fetch("http://localhost:8001/api/chickens")
+        //     .then(res => res.json())
+        //     .then(
+        //         (result) => {
+        //             // console.log(result)
+        //             this.setState({
+        //                 isLoaded: true,
+        //                 items: result
+        //             });
+        //         },
+        //         (error) => {
+        //             this.setState({
+        //                 isLoaded: true,
+        //                 error
+        //             });
+        //         }
+        //     )
+
+        this.setState({
+            isLoaded: true,
+            items: data
+        })
+    }
+
+    render() {
+        const {error, isLoaded, items} = this.state;
+
+        console.log(items)
+
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div className="eggshelf">
+                    {items.chickens.map(item => (
+                        <Chicken name={item.name} price={item.eggPrice} file={'chickens/' + item.name.toLowerCase() + '.png'}/>
+                    ))}
+                </div>
+            )
+        }
+    }
+}
+
+export default EggShelf
