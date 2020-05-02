68f3b3e2655983a66a482ff1c664d6a2503c3caa
dave <david.goldfinch@datumsolutions.net>
2020-05-02 14:51:09 -0300

    Updates

---
+++ precache-manifest.f71000cd57956d5a6d192e9569291dd1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1705c9d795cac885cf1cf27c4bf3d15",
+    "url": "/farm-website/index.html"
+  },
+  {
+    "revision": "1840fe17f6addceb7934",
+    "url": "/farm-website/static/css/main.4c65c8dc.chunk.css"
+  },
+  {
+    "revision": "274421a8c076fabedc4c",
+    "url": "/farm-website/static/js/2.024f1f44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/farm-website/static/js/2.024f1f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1840fe17f6addceb7934",
+    "url": "/farm-website/static/js/main.b6c362f3.chunk.js"
+  },
+  {
+    "revision": "fda3acdc5488a586e116",
+    "url": "/farm-website/static/js/runtime-main.4dcd9827.js"
+  },
+  {
+    "revision": "7173b662cfe8eb79cd01436c35448cd8",
+    "url": "/farm-website/static/media/Bea Egg.7173b662.jpg"
+  },
+  {
+    "revision": "87b776abd48b80fb5030ec45fa7be175",
+    "url": "/farm-website/static/media/Bea.87b776ab.jpg"
+  },
+  {
+    "revision": "ec7e9a8d8bba2ee1331ca310ceb779c2",
+    "url": "/farm-website/static/media/Belinda Egg.ec7e9a8d.jpg"
+  },
+  {
+    "revision": "a737098ef8c5c5431c349b6f268495b1",
+    "url": "/farm-website/static/media/Belinda.a737098e.jpg"
+  },
+  {
+    "revision": "079d558bc96ef3a4b0a41e8afaab67de",
+    "url": "/farm-website/static/media/Bertie Egg.079d558b.jpg"
+  },
+  {
+    "revision": "9560eaa0809a3787c4e07e13af54c213",
+    "url": "/farm-website/static/media/Bertie.9560eaa0.jpg"
+  },
+  {
+    "revision": "6317f6f3f96775a37cba968ac1f37bb7",
+    "url": "/farm-website/static/media/Brenda Egg.6317f6f3.jpg"
+  },
+  {
+    "revision": "637db767d3c8d9ac7dea7c868bf8316e",
+    "url": "/farm-website/static/media/Brenda.637db767.jpg"
+  },
+  {
+    "revision": "1cbec5dfdf4b0aaa0b31e0baee819d71",
+    "url": "/farm-website/static/media/Dottie.1cbec5df.jpg"
+  },
+  {
+    "revision": "a316e0d43c67f2b09721bcc9fad19d49",
+    "url": "/farm-website/static/media/Helen Egg.a316e0d4.jpg"
+  },
+  {
+    "revision": "7da5a0d87f610d2c112c3c7f4602889e",
+    "url": "/farm-website/static/media/Helen.7da5a0d8.jpg"
+  },
+  {
+    "revision": "55331cb99ae2e06b1d2033f9dc1e29d4",
+    "url": "/farm-website/static/media/Millie Egg.55331cb9.jpg"
+  },
+  {
+    "revision": "cded07c26670ad4d9724b8d52c0e671c",
+    "url": "/farm-website/static/media/Millie.cded07c2.jpg"
+  },
+  {
+    "revision": "94e3ceee3d3b570dbe6cab391b0117fc",
+    "url": "/farm-website/static/media/Pearl Egg.94e3ceee.jpg"
+  },
+  {
+    "revision": "f706d31c26b6e736e05428ce2227ba21",
+    "url": "/farm-website/static/media/Pearl.f706d31c.jpg"
+  },
+  {
+    "revision": "b57941dc9464242ff10ce4806647d36d",
+    "url": "/farm-website/static/media/Poppy Egg.b57941dc.jpg"
+  },
+  {
+    "revision": "989b4b0cf09ae4373e4f6b84f79cfacb",
+    "url": "/farm-website/static/media/Poppy.989b4b0c.jpg"
+  },
+  {
+    "revision": "82c65ed60ef1dbccfa356b1239ee7575",
+    "url": "/farm-website/static/media/Ruth.82c65ed6.jpg"
+  },
+  {
+    "revision": "8d2420954339276928f0b001e5b5db85",
+    "url": "/farm-website/static/media/home_hq_2.8d242095.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/farm-website/precache-manifest.91bfcf88f01fe6e82d121a361e256943.js"
+  "/farm-website/precache-manifest.f71000cd57956d5a6d192e9569291dd1.js"
 );
 
 self.addEventListener('message', (event) => {
